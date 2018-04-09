1ed86cedb57421dda92b523eb041dda6445d1d23
riteshroyrr9 <riteshroy5235@gmail.com>
2018-04-09 18:38:17 -0500

    Hello World program

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=10;i++)
+{
+System.out.println("Hello world" +i);
+}
+}
+}
+
+git config --global user.email “riteshroy5235@gmail.com"
+git config --global user.name “riteshroyrr9”
